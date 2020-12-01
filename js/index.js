99a62b615c4d1f0e3e2cd634644a0590d631ad9a
Hussain Ali <ali.415code@gmail.com>
2020-12-01 14:58:58 -0800

    updated nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// Nav
+
+const navigation = document.querySelector('.main-navigation')
+
+navigation.addEventListener('mouseover', function(event){
+    event.target.style.color = 'blue'  
+})
+navigation.addEventListener('mouseout', function(event){
+    event.target.style.color = 'black'  
+})
+
+
