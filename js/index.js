542d6a058d4edaa78b47a559a7cbb8d53209d61d
Hussain Ali <ali.415code@gmail.com>
2020-12-01 16:25:00 -0800

    updated buttons

--- js/index.js
+++ js/index.js
@@ -12,3 +12,38 @@ navigation.addEventListener('mouseout', function(event){
 })
 
 
+// body
+
+const body = document.querySelector('body')
+body.addEventListener('keydown', function(event){
+    event.target.style.background = 'yellow'
+})
+body.addEventListener('keyup', function(event){
+    event.target.style.background = 'white'
+})
+
+window.addEventListener('load', (event) => {
+    alert('page is fully loaded');
+  });
+
+window.addEventListener("dblclick", () => {
+alert("Ouch, can you stop that");
+});
+
+// intro 
+
+const intro = document.querySelector('.home')
+intro.addEventListener('wheel', function(event){
+    event.target.style.fontSize = '2rem'
+})
+
+// btn 
+
+document.body.addEventListener("click", event => {
+    if (event.target.className == "btn") {
+      console.log("Clicked", event.target.textContent);
+      event.target.style.background = 'green'
+    }
+  });
+
+//
